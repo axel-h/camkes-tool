8677a2a5ff2250b301eb60b9578b8adcac09e97f
Axel Heider <axelheider@gmx.de>
2021-08-27 02:27:17 +0200

    virtqueue: cleanup and sanity checking
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- libsel4camkes/src/virtqueue_common.c
+++ libsel4camkes/src/virtqueue_common.c
@@ -49,34 +49,78 @@ struct vq_buf_alloc *init_vq_allocator(void *mem_pool, unsigned len, size_t bloc
     return allocator;
 }
 
-int camkes_virtqueue_driver_init_common(virtqueue_driver_t *driver, volatile void *buffer, unsigned queue_len,
-                                        size_t buffer_size, void (*notify)(void), size_t block_size)
+typedef struct {
+    size_t offs_avail_ring;
+    size_t offs_used_ring;
+    size_t offs_desc;
+    size_t offs_end;
+} virtqueue_offsets_t;
+
+static virtqueue_offsets_t calulate_virtqueue_offsets(unsigned queue_len)
+{
+    virtqueue_offsets_t vqo;
+
+    size_t offs = 0;
+    vqo.offs_avail_ring = offs;
+    offs += sizeof(vq_vring_avail_t) +
+            sizeof(vq_vring_avail_elem_t) * queue_len;
+
+    vqo.offs_used_ring = offs;
+    offs += sizeof(vq_vring_used_t) +
+            sizeof(vq_vring_used_elem_t) * queue_len;
+
+    vqo.offs_desc = offs;
+    offs += sizeof(vq_vring_desc_t) * queue_len;
+
+    vqo.offs_end = offs;
+
+    return vqo;
+}
+
+int camkes_virtqueue_driver_init_common(virtqueue_driver_t *driver,
+                                        volatile void *buffer,
+                                        unsigned queue_len,
+                                        size_t buffer_size,
+                                        void (*notify)(void),
+                                        size_t block_size)
 {
     /* Don't check for notify, as it can be NULL in some situations */
     if (!driver || !buffer) {
         return EINVAL;
     }
 
-    void *avail_ring = (void *) buffer;
-    void *used_ring = avail_ring + sizeof(vq_vring_avail_t) + sizeof(uint16_t) * queue_len;
-    void *desc = used_ring + sizeof(vq_vring_used_t) + sizeof(struct vq_vring_used_elem) * queue_len;
-
-    void *usable_buffer_start = desc + sizeof(vq_vring_desc_t) * queue_len;
-    unsigned usable_len = buffer_size - (sizeof(vq_vring_avail_t) + sizeof(vq_vring_used_t) +
-                                         sizeof(uint16_t) * queue_len + sizeof(struct vq_vring_used_elem) * queue_len +
-                                         sizeof(vq_vring_desc_t) * queue_len);
-    struct vq_buf_alloc *allocator = init_vq_allocator(usable_buffer_start, usable_len, block_size);
-    if (!allocator) {
+    /* Calculate the offsets for the management structures. */
+    virtqueue_offsets_t vqo = calulate_virtqueue_offsets(queue_len);
+    /* Check if the buffer is big enough. */
+    if (vqo.offs_end >= buffer_size) {
+        ZF_LOGE("virtqueue buffer too small, have %zu, need at least %zu",
+                buffer_size, vqo.offs_end);
         return ENOMEM;
     }
 
-    virtqueue_init_driver(driver, queue_len, avail_ring, used_ring, desc, notify, allocator);
+    /* The remaining buffer is available for queue data. */
+    size_t vq_buffer_size = buffer_size - vqo.offs_end;
+    void *vq_buffer = (void *)((uintptr_t)buffer + vqo.offs_end);
+    struct vq_buf_alloc *allocator = init_vq_allocator(vq_buffer,
+                                                       vq_buffer_size,
+                                                       block_size);
+    if (!allocator) {
+        ZF_LOGE("virtqueue allocator initialization failed");
+        return ENOMEM;
+    }
 
+    void *avail_ring = (void *)((uintptr_t)buffer + vqo.offs_avail_ring);
+    void *used_ring = (void *)((uintptr_t)buffer + vqo.offs_used_ring);
+    void *desc = (void *)((uintptr_t)buffer + vqo.offs_desc);
+    virtqueue_init_driver(driver, queue_len, avail_ring, used_ring, desc,
+                          notify, allocator);
 
     return 0;
 }
 
-int camkes_virtqueue_device_init_common(virtqueue_device_t *device, volatile void *buffer, unsigned queue_len,
+int camkes_virtqueue_device_init_common(virtqueue_device_t *device,
+                                        volatile void *buffer,
+                                        unsigned queue_len,
                                         void (*notify)(void))
 {
     /* Don't check for notify, as it can be NULL in some situations */
@@ -84,11 +128,19 @@ int camkes_virtqueue_device_init_common(virtqueue_device_t *device, volatile voi
         return EINVAL;
     }
 
-    void *avail_ring = (void *) buffer;
-    void *used_ring = avail_ring + sizeof(vq_vring_avail_t) + sizeof(uint16_t) * queue_len;
-    void *desc = used_ring + sizeof(vq_vring_used_t) + sizeof(struct vq_vring_used_elem) * queue_len;
-    void *cookie = desc + sizeof(vq_vring_desc_t) * queue_len;
-    virtqueue_init_device(device, queue_len, avail_ring, used_ring, desc, notify, cookie);
+    /* Calculate the offsets for the management structures. */
+    virtqueue_offsets_t vqo = calulate_virtqueue_offsets(queue_len);
+
+    /* The buffer size is not passed, so we can only hope it is big enough for
+     * what we need.
+     */
+
+    void *avail_ring = (void *)((uintptr_t)buffer + vqo.offs_avail_ring);
+    void *used_ring = (void *)((uintptr_t)buffer + vqo.offs_used_ring);
+    void *desc = (void *)((uintptr_t)buffer + vqo.offs_desc);
+    void *cookie = (void *)((uintptr_t)buffer + vqo.offs_end);
+    virtqueue_init_device(device, queue_len, avail_ring, used_ring, desc,
+                          notify, cookie);
 
     return 0;
 }
